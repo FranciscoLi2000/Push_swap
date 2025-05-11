4c5e30d3d977a962c18ace12190d40274109837b
FranciscoLi2000 <liyufeng577@gmail.com>
2025-05-11 04:30:14 +0200

    头文件定义

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,21 +6,19 @@
 /*   By: yufli <yufli@student.42barcelona.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/10 23:37:53 by yufli             #+#    #+#             */
-/*   Updated: 2025/05/11 02:27:14 by yufli            ###   ########.fr       */
+/*   Updated: 2025/05/11 04:29:50 by yufli            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-# include <unistd.h>
-# include <stdlib.h>
-# include <limits.h>
+# include "../libft/libft.h"
 
-typedef struct s_stack	/* 定义栈结构 */
+typedef struct s_stack
 {
-	int	*numbers;	/* 存储整数的数组 */
-	int	size;		/* 栈中元素数量 */
+	int	*numbers;
+	int	size;
 }	t_stack;
 
 /* 基本操作函数 */
@@ -42,15 +40,16 @@ void	sort_two(t_stack *stack);
 void	sort_three(t_stack *stack);
 void	sort_five_or_less(t_stack *stack_a, t_stack *stack_b);
 void	radix_sort(t_stack *stack_a, t_stack *stack_b);
+void	convert_to_indices(t_stack *stack);
 
 /* 辅助函数 */
-int		ft_is_sorted(t_stack *stack);
-void	convert_to_indices(t_stack *stack);
-long	ft_strtol(const char *str);
-int		is_valid_number(char *str);
+int		is_sorted(t_stack *stack);
 int		check_duplicates(t_stack *stack, int num, int index);
 void	print_error(void);
 void	free_stacks(t_stack *stack_a, t_stack *stack_b);
 int		parse_arguments(int argc, char **argv, t_stack *a, t_stack *b);
+int		is_valid_number(char *str);
+int		*create_copy(t_stack *stack);
+int		find_index(int *sorted, int value, int size);
 
 #endif
